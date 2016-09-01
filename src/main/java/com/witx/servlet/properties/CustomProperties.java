622ed64c08728142265125ea8ed0adc56baab093
LxzLLL <minepop09@163.com>
2016-09-01 19:22:57 +0800

    调整引用的包

--- src/main/java/com/witx/servlet/properties/CustomProperties.java
+++ src/main/java/com/witx/servlet/properties/CustomProperties.java
@@ -1,11 +1,9 @@
 package com.witx.servlet.properties;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Properties;
 import java.util.Enumeration;
 import java.util.Map.Entry;
+import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
