db7440f77d17ae5e4e1c428712621c887c30b5e7
LxzLLL <minepop09@163.com>
2016-09-03 19:17:25 +0800

    增加拦截器其他通知

--- src/main/java/com/witx/interceptor/FirstInterceptor.java
+++ src/main/java/com/witx/interceptor/FirstInterceptor.java
@@ -1,5 +1,10 @@
 package com.witx.interceptor;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -22,5 +27,45 @@ public class FirstInterceptor {
 	public void before(){
 		System.out.println("我是前置通知");
 	}
+	@AfterReturning("anyMethod()")
+	public void afterReturning(){
+		System.out.println("我是后置通知");
+	}
 	
+	@After("anyMethod()")
+	public void after(){
+		System.out.println("我是最终通知");
+	}
+	
+	
+	@AfterThrowing("anyMethod()")
+	public void afterThrowing(){
+		System.out.println("我是例外通知");
+	}
+	
+	
+	
+	/**
+	 * 环绕通知，可以共享变量
+	 * @param pjp
+	 * @throws Throwable
+	 */
+	@Around("anyMethod()")
+	public Object doBasicProfiling(ProceedingJoinPoint pjp) throws Throwable{
+		//Object result = pjp.proceed();    //必须执行此语句
+		Object result = null;
+		try{
+			System.out.println("begin!");
+			long start = System.currentTimeMillis();
+			result = pjp.proceed();
+			long end = System.currentTimeMillis();
+			
+			System.out.println("end 耗时："+(end-start)+" milliseconds");
+			
+		}
+		catch(Throwable e){
+			System.out.println("异常");
+		}
+		return result;
+	}
 }
